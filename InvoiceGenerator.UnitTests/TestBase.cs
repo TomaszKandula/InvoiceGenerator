0d4d6305e52fd1d52224fd04cf9428e6e7c65c43
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-13 16:56:37 +0100

    feat: add database context and test base for UnitTest project

---
+++ InvoiceGenerator.UnitTests/TestBase.cs
@@ -0,0 +1,36 @@
+﻿namespace InvoiceGenerator.UnitTests
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Backend.Database;
+    using Backend.Core.Services.DateTimeService;
+    using Backend.Core.Services.DataUtilityService;
+
+    public class TestBase
+    {
+        protected IDataUtilityService DataUtilityService { get; }
+
+        protected IDateTimeService DateTimeService { get; }
+        
+        private readonly DatabaseContextFactory _databaseContextFactory;
+        
+        protected TestBase()
+        {
+            DataUtilityService = new DataUtilityService();
+            DateTimeService = new DateTimeService();
+
+            var services = new ServiceCollection();
+            services.AddSingleton<DatabaseContextFactory>();
+            services.AddScoped(context =>
+            {
+                var factory = context.GetService<DatabaseContextFactory>();
+                return factory?.CreateDatabaseContext();
+            });
+
+            var serviceScope = services.BuildServiceProvider(true).CreateScope();
+            var serviceProvider = serviceScope.ServiceProvider;
+            _databaseContextFactory = serviceProvider.GetService<DatabaseContextFactory>();
+        }
+
+        protected DatabaseContext GetTestDatabaseContext() =>  _databaseContextFactory.CreateDatabaseContext();
+    }
+}
