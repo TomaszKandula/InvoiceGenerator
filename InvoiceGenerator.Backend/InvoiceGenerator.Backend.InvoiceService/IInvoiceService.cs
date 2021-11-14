4ef37d46a766115eadde9952eed8495433abb1f9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-14 14:42:16 +0100

    feat: add two new implementations for invoice service

--- InvoiceGenerator.Backend/InvoiceGenerator.Backend.InvoiceService/IInvoiceService.cs
+++ InvoiceGenerator.Backend/InvoiceGenerator.Backend.InvoiceService/IInvoiceService.cs
@@ -1,7 +1,15 @@
 namespace InvoiceGenerator.Backend.InvoiceService
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using Models;
+
     public interface IInvoiceService
     {
-        
+        Task<Guid> OrderInvoiceBatchProcessing(IEnumerable<OrderDetail> orderDetails, CancellationToken cancellationToken = default);
+
+        Task<ProcessingStatus> GetBatchInvoiceProcessingStatus(Guid processBatchKey, CancellationToken cancellationToken = default);
     }
 }
