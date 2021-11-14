4c1027fe107376df5e3698c51c0690355ac902cf
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-14 12:19:57 +0100

    refactor: change namings

--- InvoiceGenerator.Backend/InvoiceGenerator.Backend.Domain/Enums/InvoiceProcessingStatus.cs
+++ InvoiceGenerator.Backend/InvoiceGenerator.Backend.Domain/Enums/InvoiceProcessingStatus.cs
@@ -2,9 +2,9 @@ namespace InvoiceGenerator.Backend.Domain.Enums
 {
     public enum InvoiceProcessingStatus
     {
-        NEW,
-        INVOICE_GENERATING_STARTED,
-        INVOICE_GENERATING_FINISHED,
-        FAILED
+        New,
+        InvoiceGeneratingStarted,
+        InvoiceGeneratingFinished,
+        Failed
     }
 }
