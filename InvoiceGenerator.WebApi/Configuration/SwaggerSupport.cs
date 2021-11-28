b52c862bd68c3f08fa53e789b52bd5de7c9f6336
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-28 17:51:55 +0100

    refactor: simplify configuration code

--- InvoiceGenerator.WebApi/Configuration/SwaggerSupport.cs
+++ InvoiceGenerator.WebApi/Configuration/SwaggerSupport.cs
@@ -3,17 +3,21 @@ namespace InvoiceGenerator.WebApi.Configuration
     using System.Diagnostics.CodeAnalysis;
     using Microsoft.OpenApi.Models;
     using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.DependencyInjection;
 
     [ExcludeFromCodeCoverage]
-    public static class Swagger
+    public static class SwaggerSupport
     {
         private const string ApiVersion = "v1";
 
         private const string ApiName = "Invoice Generator API";
         
-        public static void SetupSwaggerOptions(this IServiceCollection services)
+        public static void SetupSwaggerOptions(this IServiceCollection services, IHostEnvironment environment)
         {
+            if (environment.IsProduction()) 
+                return;            
+            
             services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc(ApiVersion, new OpenApiInfo
@@ -24,8 +28,12 @@ namespace InvoiceGenerator.WebApi.Configuration
             });
         }
 
-        public static void SetupSwaggerUi(this IApplicationBuilder builder)
+        public static void SetupSwaggerUi(this IApplicationBuilder builder, IHostEnvironment environment)
         {
+            if (environment.IsProduction()) 
+                return;            
+
+            builder.UseSwagger();
             builder.UseSwaggerUI(options =>
             {
                 options.SwaggerEndpoint($"/swagger/{ApiVersion}/swagger.json", ApiName);
