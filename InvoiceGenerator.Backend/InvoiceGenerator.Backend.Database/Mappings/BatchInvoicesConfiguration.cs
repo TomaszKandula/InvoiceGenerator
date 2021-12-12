6e22dd221bb94b784eba9020335cd81d07c8e33f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-12-12 16:03:33 +0100

    feat: add configuration for new constraints

--- InvoiceGenerator.Backend/InvoiceGenerator.Backend.Database/Mappings/BatchInvoicesConfiguration.cs
+++ InvoiceGenerator.Backend/InvoiceGenerator.Backend.Database/Mappings/BatchInvoicesConfiguration.cs
@@ -18,6 +18,27 @@ namespace InvoiceGenerator.Backend.Database.Mappings
                 .HasForeignKey(invoices => invoices.ProcessBatchKey)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_BatchInvoices_BatchInvoicesProcessing");
+
+            builder
+                .HasOne(invoices => invoices.Users)
+                .WithMany(users => users.BatchInvoices)
+                .HasForeignKey(invoices => invoices.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_BatchInvoices_Users");
+
+            builder
+                .HasOne(invoices => invoices.UserDetails)
+                .WithMany(details => details.BatchInvoices)
+                .HasForeignKey(invoices => invoices.UserDetailId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_BatchInvoices_UserDetails");
+
+            builder
+                .HasOne(invoices => invoices.UserBankData)
+                .WithMany(bankData => bankData.BatchInvoices)
+                .HasForeignKey(invoices => invoices.UserBankDataId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_BatchInvoices_UserBankData");
         }
     }
 }
