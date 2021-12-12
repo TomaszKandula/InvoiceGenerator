171eb5c5f94913481b53bb019e82aa4137e3d92b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-12-12 19:03:59 +0100

    fix: change incorrect file upload implementation

--- InvoiceGenerator.Backend/InvoiceGenerator.Backend.Shared/Dto/ReplaceInvoiceTemplateDto.cs
+++ InvoiceGenerator.Backend/InvoiceGenerator.Backend.Shared/Dto/ReplaceInvoiceTemplateDto.cs
@@ -1,14 +1,17 @@
 namespace InvoiceGenerator.Backend.Shared.Dto
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Http;
     using Shared.Models;
 
     public class ReplaceInvoiceTemplateDto : RequestProperties
     {
         public Guid Id { get; set; }
 
-        public byte[] Data { get; set; }
+        public string Description { get; set; }
 
-        public string DataType { get; set; }
+        [DataType(DataType.Upload)]
+        public IFormFile Data { get; set; }
     }
 }
