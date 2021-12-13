164b5d853f97b1d17f6d5a5173afdc7fe8fc4537
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-12-13 20:52:13 +0100

    docs: update XML comment, add numeric values

--- InvoiceGenerator.Backend/InvoiceGenerator.Backend.Domain/Enums/CountryCodes.cs
+++ InvoiceGenerator.Backend/InvoiceGenerator.Backend.Domain/Enums/CountryCodes.cs
@@ -4,97 +4,104 @@ namespace InvoiceGenerator.Backend.Domain.Enums
     using Newtonsoft.Json;
     using Core.Converters;
 
+    /// <summary>
+    /// Country codes. Numeric values follows UN Code.
+    /// </summary>
+    /// <remarks>
+    /// This is a incomplete list of country ISO codes as described in the ISO 3166 international standard.
+    /// In case of extending this list, please keep the correct numeric value.
+    /// </remarks>
     [JsonConverter(typeof(StringToEnumWithDefaultConverter))]
     public enum CountryCodes
     {
         [EnumMember(Value = "unknown")]
-        Unknown,
+        Unknown = 0,
 
         [EnumMember(Value = "austria")]
-        Austria,
+        Austria = 40,
 
         [EnumMember(Value = "belgium")]
-        Belgium,
+        Belgium = 56,
 
         [EnumMember(Value = "bulgaria")]
-        Bulgaria,
+        Bulgaria = 100,
 
         [EnumMember(Value = "croatia")]
-        Croatia,
+        Croatia = 191,
 
         [EnumMember(Value = "cyprus")]
-        Cyprus,
+        Cyprus = 196,
 
         [EnumMember(Value = "czech")]
-        Czech,
+        Czech = 203,
 
         [EnumMember(Value = "denmark")]
-        Denmark,
+        Denmark = 208,
 
         [EnumMember(Value = "estonia")]
-        Estonia,
+        Estonia = 233,
 
         [EnumMember(Value = "finland")]
-        Finland,
+        Finland = 246,
 
         [EnumMember(Value = "france")]
-        France,
+        France = 250,
 
         [EnumMember(Value = "germany")]
-        Germany,
+        Germany = 276,
 
         [EnumMember(Value = "greece")]
-        Greece,
+        Greece = 300,
 
         [EnumMember(Value = "hungary")]
-        Hungary,
+        Hungary = 348,
 
         [EnumMember(Value = "ireland")]
-        Ireland,
+        Ireland = 372,
 
         [EnumMember(Value = "italy")]
-        Italy,
+        Italy = 380,
 
         [EnumMember(Value = "latvia")]
-        Latvia,
+        Latvia = 428,
 
         [EnumMember(Value = "lithuania")]
-        Lithuania,
+        Lithuania = 440,
 
         [EnumMember(Value = "luxembourg")]
-        Luxembourg,
+        Luxembourg = 442,
 
         [EnumMember(Value = "malta")]
-        Malta,
+        Malta = 470,
 
         [EnumMember(Value = "netherlands")]
-        Netherlands,
+        Netherlands = 528,
 
         [EnumMember(Value = "poland")]
-        Poland,
+        Poland = 616,
 
         [EnumMember(Value = "portugal")]
-        Portugal,
+        Portugal = 620,
 
         [EnumMember(Value = "romania")]
-        Romania,
+        Romania = 642,
 
         [EnumMember(Value = "slovakia")]
-        Slovakia,
+        Slovakia = 703,
 
         [EnumMember(Value = "slovenia")]
-        Slovenia,
+        Slovenia = 705,
 
         [EnumMember(Value = "spain")]
-        Spain,
+        Spain = 724,
 
         [EnumMember(Value = "sweden")]
-        Sweden,
+        Sweden = 752,
         
         [EnumMember(Value = "usa")]
-        Usa,
+        Usa = 840,
         
         [EnumMember(Value = "china")]
-        China
+        China = 156
     }
 }
