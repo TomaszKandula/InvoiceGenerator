df94046c915392a0bf493133dc365545c41844c9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-12-13 12:22:47 +0100

    feat: add currency code to user bank accounts table

--- InvoiceGenerator.Backend/InvoiceGenerator.Backend.Domain/Entities/UserBankAccounts.cs
+++ InvoiceGenerator.Backend/InvoiceGenerator.Backend.Domain/Entities/UserBankAccounts.cs
@@ -4,6 +4,7 @@ namespace InvoiceGenerator.Backend.Domain.Entities
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.ComponentModel.DataAnnotations;
+    using Enums;
 
     [ExcludeFromCodeCoverage]
     public class UserBankAccounts : Entity<Guid>
@@ -21,6 +22,9 @@ namespace InvoiceGenerator.Backend.Domain.Entities
         [Required]
         [MaxLength(28)]
         public string AccountNumber { get; set; }
+
+        [Required]
+        public CurrencyCodes CurrencyCode { get; set; }
 
         public Users User { get; set; }
 
