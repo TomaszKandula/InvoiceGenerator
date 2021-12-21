860aac308f624f570050f6abb4cdbc3aa4ddbb8b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-12-21 20:45:40 +0100

    refactor: use ILogger

--- InvoiceGenerator.Workers/InvoiceProcessingWorker.cs
+++ InvoiceGenerator.Workers/InvoiceProcessingWorker.cs
@@ -3,30 +3,25 @@ namespace InvoiceGenerator.Workers
     using System.Diagnostics;
     using System.Threading.Tasks;
     using Microsoft.Azure.WebJobs;
+    using Microsoft.Extensions.Logging;
     using Backend.BatchService;
-    using Backend.Core.Services.LoggerService;
 
     public class InvoiceProcessingWorker
     {
         private readonly IBatchService _batchService;
-        private readonly ILoggerService _loggerService;
         
-        public InvoiceProcessingWorker(IBatchService batchService, ILoggerService loggerService)
-        {
-            _batchService = batchService;
-            _loggerService = loggerService;
-        }
+        public InvoiceProcessingWorker(IBatchService batchService) => _batchService = batchService;
 
         [FunctionName("ProcessOutstandingInvoices")]
-        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer)
+        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger logger)
         {
             var timer = new Stopwatch();
 
             timer.Start();
-            _loggerService.LogInformation("Starting the processing of outstanding invoices...");
+            logger.LogInformation("Starting the processing of outstanding invoices...");
             await _batchService.ProcessOutstandingInvoices();
             timer.Stop();
-            _loggerService.LogInformation($"All outstanding invoices has been processed within: {timer.Elapsed}.");
+            logger.LogInformation("All outstanding invoices has been processed within: {TimeElapsed}", timer.Elapsed);
         }
     }
 }
