4e6b1508c6f96a124abde3acee40c1626f684309
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-12-25 23:02:07 +0100

    refactor: remove braces

--- InvoiceGenerator.Workers/InvoiceProcessingWorker.cs
+++ InvoiceGenerator.Workers/InvoiceProcessingWorker.cs
@@ -1,27 +1,26 @@
-namespace InvoiceGenerator.Workers
+namespace InvoiceGenerator.Workers;
+
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Backend.BatchService;
+
+public class InvoiceProcessingWorker
 {
-    using System.Diagnostics;
-    using System.Threading.Tasks;
-    using Microsoft.Azure.WebJobs;
-    using Microsoft.Extensions.Logging;
-    using Backend.BatchService;
-
-    public class InvoiceProcessingWorker
-    {
-        private readonly IBatchService _batchService;
+    private readonly IBatchService _batchService;
         
-        public InvoiceProcessingWorker(IBatchService batchService) => _batchService = batchService;
+    public InvoiceProcessingWorker(IBatchService batchService) => _batchService = batchService;
 
-        [FunctionName("ProcessOutstandingInvoices")]
-        public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger logger)
-        {
-            var timer = new Stopwatch();
+    [FunctionName("ProcessOutstandingInvoices")]
+    public async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger logger)
+    {
+        var timer = new Stopwatch();
 
-            timer.Start();
-            logger.LogInformation("Starting the processing of outstanding invoices...");
-            await _batchService.ProcessOutstandingInvoices();
-            timer.Stop();
-            logger.LogInformation("All outstanding invoices has been processed within: {TimeElapsed}", timer.Elapsed);
-        }
+        timer.Start();
+        logger.LogInformation("Starting the processing of outstanding invoices...");
+        await _batchService.ProcessOutstandingInvoices();
+        timer.Stop();
+        logger.LogInformation("All outstanding invoices has been processed within: {TimeElapsed}", timer.Elapsed);
     }
 }
