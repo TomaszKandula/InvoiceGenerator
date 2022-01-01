a87502a7093665c70c0e831d714b3c8c097ef585
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-01-01 21:30:25 +0100

    feat: add private key header for all endpoints

--- InvoiceGenerator.WebApi/Controllers/PaymentsController.cs
+++ InvoiceGenerator.WebApi/Controllers/PaymentsController.cs
@@ -14,21 +14,21 @@ public class PaymentsController : BaseController
 
     [HttpGet]
     [ProducesResponseType(typeof(IEnumerable<GetPaymentTypeListQueryResult>), StatusCodes.Status200OK)]
-    public async Task<IEnumerable<GetPaymentTypeListQueryResult>> GetPaymentTypeList([FromQuery] string privateKey) =>
-        await Mediator.Send(new GetPaymentTypeListQuery { PrivateKey = privateKey, FilterBy = string.Empty });
+    public async Task<IEnumerable<GetPaymentTypeListQueryResult>> GetPaymentTypeList([FromHeader(Name = HeaderName)] string privateKey) =>
+        await Mediator.Send(new GetPaymentTypeListQuery { FilterBy = string.Empty });
 
     [HttpGet("{type}")]
     [ProducesResponseType(typeof(IEnumerable<GetPaymentTypeListQueryResult>), StatusCodes.Status200OK)]
-    public async Task<IEnumerable<GetPaymentTypeListQueryResult>> GetPaymentType([FromRoute] string type, [FromQuery] string privateKey) =>
-        await Mediator.Send(new GetPaymentTypeListQuery { PrivateKey = privateKey, FilterBy = type });
+    public async Task<IEnumerable<GetPaymentTypeListQueryResult>> GetPaymentType([FromRoute] string type, [FromHeader(Name = HeaderName)] string privateKey) =>
+        await Mediator.Send(new GetPaymentTypeListQuery { FilterBy = type });
 
     [HttpGet]
     [ProducesResponseType(typeof(IEnumerable<GetPaymentStatusListQueryResult>), StatusCodes.Status200OK)]
-    public async Task<IEnumerable<GetPaymentStatusListQueryResult>> GetPaymentStatusList([FromQuery] string privateKey, string status) =>
-        await Mediator.Send(new GetPaymentStatusListQuery { PrivateKey = privateKey, FilterBy = status });
+    public async Task<IEnumerable<GetPaymentStatusListQueryResult>> GetPaymentStatusList([FromQuery] string status, [FromHeader(Name = HeaderName)] string privateKey) =>
+        await Mediator.Send(new GetPaymentStatusListQuery { FilterBy = status });
 
     [HttpGet("{status}")]
     [ProducesResponseType(typeof(IEnumerable<GetPaymentStatusListQueryResult>), StatusCodes.Status200OK)]
-    public async Task<IEnumerable<GetPaymentStatusListQueryResult>> GetPaymentStatusCode([FromRoute] string status, [FromQuery] string privateKey) =>
-        await Mediator.Send(new GetPaymentStatusListQuery { PrivateKey = privateKey, FilterBy = status });
+    public async Task<IEnumerable<GetPaymentStatusListQueryResult>> GetPaymentStatusCode([FromRoute] string status, [FromHeader(Name = HeaderName)] string privateKey) =>
+        await Mediator.Send(new GetPaymentStatusListQuery { FilterBy = status });
 }
--- InvoiceGenerator.WebApi/Controllers/TemplatesController.cs
+++ InvoiceGenerator.WebApi/Controllers/TemplatesController.cs
@@ -19,26 +19,26 @@ public class TemplatesController : BaseController
 
     [HttpGet]
     [ProducesResponseType(typeof(IEnumerable<InvoiceTemplateInfo>), StatusCodes.Status200OK)]
-    public async Task<IEnumerable<InvoiceTemplateInfo>> GetInvoiceTemplates([FromQuery] string privateKey) 
-        => await Mediator.Send(new GetInvoiceTemplatesQuery { PrivateKey = privateKey });
+    public async Task<IEnumerable<InvoiceTemplateInfo>> GetInvoiceTemplates([FromHeader(Name = HeaderName)] string privateKey) 
+        => await Mediator.Send(new GetInvoiceTemplatesQuery());
 
     [HttpGet("{id:guid}")]
     [ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
-    public async Task<FileContentResult> GetInvoiceTemplate([FromRoute] Guid id, [FromQuery] string privateKey) 
-        => await Mediator.Send(new GetInvoiceTemplateQuery { PrivateKey = privateKey, Id = id});
+    public async Task<FileContentResult> GetInvoiceTemplate([FromRoute] Guid id, [FromHeader(Name = HeaderName)] string privateKey) 
+        => await Mediator.Send(new GetInvoiceTemplateQuery { Id = id});
 
     [HttpPost]
     [ProducesResponseType(typeof(AddInvoiceTemplateCommandResult), StatusCodes.Status200OK)]
-    public async Task<AddInvoiceTemplateCommandResult> AddInvoiceTemplate([FromForm] AddInvoiceTemplateDto payload)
+    public async Task<AddInvoiceTemplateCommandResult> AddInvoiceTemplate([FromForm] AddInvoiceTemplateDto payload, [FromHeader(Name = HeaderName)] string privateKey)
         => await Mediator.Send(TemplatesMapper.MapToAddInvoiceTemplateCommandRequest(payload));
 
     [HttpPost]
     [ProducesResponseType(typeof(Unit), StatusCodes.Status200OK)]
-    public async Task<Unit> ReplaceInvoiceTemplate([FromForm] ReplaceInvoiceTemplateDto payload) 
+    public async Task<Unit> ReplaceInvoiceTemplate([FromForm] ReplaceInvoiceTemplateDto payload, [FromHeader(Name = HeaderName)] string privateKey) 
         => await Mediator.Send(TemplatesMapper.MapToReplaceInvoiceTemplateCommandRequest(payload));
 
     [HttpPost]
     [ProducesResponseType(typeof(Unit), StatusCodes.Status200OK)]
-    public async Task<Unit> RemoveInvoiceTemplate([FromBody] RemoveInvoiceTemplateDto payload) 
+    public async Task<Unit> RemoveInvoiceTemplate([FromBody] RemoveInvoiceTemplateDto payload, [FromHeader(Name = HeaderName)] string privateKey) 
         => await Mediator.Send(TemplatesMapper.MapToRemoveInvoiceTemplateCommandRequest(payload));
 }
