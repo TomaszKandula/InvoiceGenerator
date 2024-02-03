81258aa8cfa313e276edaea7b30b681ea19314f4
Tom Kandula <tom@tomkandula.com>
2024-02-03 17:13:44 +0100

    refactor: remove unused files

--- InvoiceGenerator.Backend/InvoiceGenerator.Backend.Cqrs/Handlers/Queries/Logger/GetLogFileContentQuery.cs
+++
@@ -1,9 +0,0 @@
-namespace InvoiceGenerator.Backend.Cqrs.Handlers.Queries.Logger;
-
-using Microsoft.AspNetCore.Mvc;
-using MediatR;
-
-public class GetLogFileContentQuery : IRequest<FileContentResult>
-{
-    public string LogFileName { get; set; }
-}
